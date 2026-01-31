49f3676d7bb61b11988cb8e71887b6d203b91277
Felipe Viera Klein <felipe.viera@mayor.cl>
2026-01-30 21:56:35 -0300

    Just coded bite() and idle() states to enemy

---
+++ godot/Enemy/EnemyGuy.gd
@@ -0,0 +1,45 @@
+extends CharacterBody3D
+
+var current_state = "idle"
+var next_state = "idle"
+var previous_state
+
+
+func _physics_process(delta):
+	if previous_state != current_state:
+		$StateLabel.text = current_state
+	
+	previous_state = current_state
+	current_state = next_state
+	
+	match current_state:
+		"idle":
+			idle()
+		#"chase":
+			#chase()
+		"bite":
+			bite()
+	
+func idle():
+	if previous_state != current_state:
+		$Enemy/AnimationPlayer.play("Rest")
+	
+	
+func bite():
+	if previous_state != current_state:
+		$Enemy/AnimationPlayer.play("Bite")
+	
+#func chase():
+	#print("We are chasing")
+	#if Input.is_action_just_pressed("a"):
+		#next_state = "idle"
+	## All of our movement code would be here
+	
+func _on_area_3d_body_entered(body):
+	if body.is_in_group("player"):
+		next_state = "bite"
+
+
+func _on_area_3d_body_exited(body):
+	if body.is_in_group("player"):
+		next_state = "idle"
